e1ff92d54ba83f7cc61bea6f5bf59acf3fcb71ec
zayscue <ztayscue@gmail.com>
2017-09-27 20:09:22 -0400

    Initial Code Commit

---
+++ Models/Message.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Models
+{
+    [Serializable]
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public MessageType Type { get; set; }
+        public string Description { get; set; }
+
+        public Message()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Message(byte[] param)
+        {
+            Deserialize(param);
+        }
+
+        public byte[] Serialize()
+        {
+            using (var stream = new MemoryStream())
+            {
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, this);
+                return stream.ToArray();
+            }
+        }
+
+        private void Deserialize(byte[] param)
+        {
+            using (var stream = new MemoryStream(param))
+            {
+                IFormatter formatter = new BinaryFormatter();
+                var temp = formatter.Deserialize(stream) as Message;
+                if(temp == null) throw new NullReferenceException(nameof(temp));
+                Id = temp.Id;
+                Type = temp.Type;
+                Description = temp.Description;
+            }
+        }
+    }
+
+    [Serializable]
+    public enum MessageType
+    {
+        Info, 
+        Error
+    }
+}
---
+++ Publisher.Core/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Client;
+using Models;
+
+namespace Publisher.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                var message = new Message
+                {
+                    Type = MessageType.Info,
+                    Description = GetMessage(args)
+                };
+                channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: message.Serialize());
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        public static string GetMessage(string[] args)
+            => args.Length > 0 ? string.Join(" ", args) : "info: Hello World!";
+    }
+}
