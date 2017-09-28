02249e4a5eae1f6b84903fafafe10d48dc29be5a
Zackery Ayscue <zayscue@ecslimited.com>
2017-09-28 15:15:28 -0400

    Added Stub Signalr Project and Changed Framework Version to 4.6.1

---
+++ Broadcaster.Framework/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Broadcaster.Framework.Startup))]
+
+namespace Broadcaster.Framework
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
